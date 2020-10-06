76e9f1f436c676b722cc5d170adb91304e493da5
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-06 17:37:58 -0400

    Update everyday-app.js

--- everyday-app.js
+++ everyday-app.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-	ReactDOM.render(
-		<h1>Hello, world!</h1>,
-		document.getElementById('root')
-	);
+	const e = React.createElement;
+
+	class LikeButton extends React.Component {
+  		constructor(props) {
+    		super(props);
+    		this.state = { liked: false };
+  		}
+
+  		render() {
+    		if (this.state.liked) {
+      			return 'You liked this.';
+    		}
+
+   			return e(
+      			'button',
+      			{ onClick: () => this.setState({ liked: true }) },
+      			'Like'
+    		);
+  		}
+	}
+	
+	const domContainer = document.querySelector('#like_button_container');ReactDOM.render(e(LikeButton), domContainer);
 });
