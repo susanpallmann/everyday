16f1e4a63da5049b9bfc26f6abdb68417910edf3
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 10:37:14 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	$('#nav-log-in').click(function(e) {
+	$('.nav-log-in').click(function(e) {
 		$('#splash-start').addClass('hide');
 		$('#splash-log-in').removeClass('hide');
 		$('#splash-sign-up').addClass('hide');
 		e.preventDefault();
 	});
 	
-	$('#nav-sign-up').click(function(e) {
+	$('.nav-sign-up').click(function(e) {
 		$('#splash-start').addClass('hide');
 		$('#splash-log-in').addClass('hide');
 		$('#splash-sign-up').removeClass('hide');
 		e.preventDefault();
 	});
 	
-	$('#nav-back').click(function(e) {
+	$('.nav-back').click(function(e) {
 		$('#splash-log-in').addClass('hide');
 		$('#splash-start').removeClass('hide');
 		$('#splash-sign-up').addClass('hide');
