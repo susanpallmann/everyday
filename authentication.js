bd00f85501b0dadf5bcd9586fef594c38eff6182
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-06 23:13:10 -0400

    Create authentication.js

---
+++ authentication.js
@@ -0,0 +1,22 @@
+// Creates a new user in Firebase Authentication with provided email and password.
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+// Logs in an existing user with Firebase Authentication given a provided email and password.
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+// Signs out an existing user from Firebase Authentication.
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
