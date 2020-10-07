e68253001eda6b5be0503f3fe4a8d72e92d109a2
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-06 23:37:08 -0400

    Update everyday-app.js

--- everyday-app.js
+++ everyday-app.js
@@ -82,18 +82,5 @@ $(document).ready(function() {
 	    document.getElementById('calendar')
 	);*/
 	
-	
-	
-	// Observes to determine if user is logged in or not.
-	firebase.auth().onAuthStateChanged(function(user) {
- 		if (user) {
-    		// User is signed in.
-			console.log("test");
-  		} else {
-    		// No user is signed in.
-			console.log("test2");
-  		}
-	});
-	
 });
 /* Magic Comment | */
