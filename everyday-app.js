e409ae4ff325311ac619e719549c1be1986d57c0
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 15:42:35 -0400

    Update everyday-app.js

--- everyday-app.js
+++ everyday-app.js
@@ -35,7 +35,7 @@ function populateDataOverview(date, data) {
     		}, r("a", {
       			href: "",
       			class: "previous-day",
-			onClick: e => this.loadPreviousDay(e)
+			onClick: e => {this.loadPreviousDay(e)}
 				
     		}, r("span", {
       			class: "material-icons"
@@ -49,7 +49,7 @@ function populateDataOverview(date, data) {
     		}, r("a", {
       			href: "",
       			class: "next-day",
-			onClick: e => this.loadNextDay(e)
+			onClick: e => {this.loadNextDay(e)}
 				
     		}, r("span", {
       			class: "material-icons"
