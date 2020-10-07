34039e8ca2439fab676a4b38102b1b4100083303
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 01:13:52 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -4,3 +4,7 @@ $(document).ready(function() {
 function appendError(element, message) {
 	element.append('<p class="error-message">' + message + '</p>');
 }
+
+function clearError(element) {
+	element.find('.error-message').remove();
+}
