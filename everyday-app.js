dee5abaf0ecaa8824168a4e6b54e89e70267a955
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 12:43:10 -0400

    Update everyday-app.js

--- everyday-app.js
+++ everyday-app.js
@@ -1,72 +1,7 @@
 /* Shorthand notation to make life easier */
 const r = React.createElement;
 
-$(document).ready(function() {
-	
-	class Hello extends React.Component {
-		render() {
-			return r('h1', null, `Hello ${this.props.toWhat}`);
-		}
-	}
-
-	ReactDOM.render(
-	    r(Hello, {toWhat: 'World'}, null),
-	    document.getElementById('app')
-	);
-	
-	class Day extends React.Component {
-		render() {
-			return r('div', {
-				class: this.props.class,
-				valid: this.props.valid,
-				filled: this.props.filled,
-				mood: this.props.mood,
-			});
-		}
-	}
-	
-	var dayArray = [
-		{
-			valid: "false",
-			filled: "false",
-			mood: null
-		},
-		{
-			valid: "true",
-			filled: "true",
-			mood: 5
-		},
-		{
-			valid: "true",
-			filled: "false",
-			mood: null
-		},
-		{
-			valid: "true",
-			filled: "true",
-			mood: 3
-		}
-	];
-	
-	var dayElements = dayArray.map(day => r(Day, {
-			class: "day",
-			valid: day['valid'],
-			filled: day['filled'],
-			mood: day['mood']
-		}));
-
-
-	ReactDOM.render(
-		r("div", null, dayElements),
-		document.getElementById('calendar')
-	);
-	
-	/*ReactDOM.render(
-	    // Element
-	    r(Day(null), null, null),
-	    document.getElementById('calendar')
-	);*/
-	
+$(document).ready(function() {	
 });
 
 function iconLookup(type) {
