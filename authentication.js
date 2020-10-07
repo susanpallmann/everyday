05d6178e0e743906a4d1b4d32c8939283b538f6e
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-06 23:36:59 -0400

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -27,8 +27,6 @@ function logOutUser {
   });
 }
 
-/* Note: this part is called in everyday-app.js because it's used with React to change the webpage to/from the splash screen.
-
 // Observes to determine if user is logged in or not.
 function checkLogInStatus() {
   firebase.auth().onAuthStateChanged(function(user) {
@@ -39,4 +37,3 @@ function checkLogInStatus() {
     }
   });
 }
-*/
