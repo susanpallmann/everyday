6460143351d170374521b9cae4ac02c37f37edf6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 00:19:29 -0400

    Update everyday-app.js

--- everyday-app.js
+++ everyday-app.js
@@ -3,21 +3,6 @@ $(document).ready(function() {
 	/* Shorthand notation to make life easier */
 	const r = React.createElement;
 	
-	/* Just for my reference for now:
-	
-	class Hello extends React.Component {
-  		render() {
-    		return <div>Hello {this.props.toWhat}</div>;
-  		}
-	}
-
-	ReactDOM.render(
-  		<Hello toWhat="World" />,
-  		document.getElementById('root')
-	);
-
-	*/
-	
 	class Hello extends React.Component {
 		render() {
 			return r('h1', null, `Hello ${this.props.toWhat}`);
