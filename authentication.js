69602274f1e9e71239def556f6eb71f7bc529f7c
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 00:36:12 -0400

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -1,24 +1,23 @@
 // Signs out an existing user from Firebase Authentication.
 function logOutUser() {
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
 }
 
 // Observes to determine if user is logged in or not.
 function checkLogInStatus() {
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      $('body').attr('splash', 'false');
-    } else {
-      // No user is signed in.
-      $('body').attr('splash', 'true');
-    }
-  });
-  checkLogInStatus();
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            $('body').attr('splash', 'false');
+        } else {
+            // No user is signed in.
+            $('body').attr('splash', 'true');
+        }
+    });
 }
 
 $(document).ready(function() {
@@ -43,5 +42,15 @@ $(document).ready(function() {
         });
         e.preventDefault();
     });
+  
+    $("#log-out").submit(function(e){
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+        }).catch(function(error) {
+            // An error happened.
+        });
+        e.preventDefault();
+    });
+    
     checkLogInStatus();
 });
