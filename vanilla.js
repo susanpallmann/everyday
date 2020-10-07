b1db249ccb71e16706b6c594e145cd3d83184942
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 02:00:26 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	$('#nav-log-in').click(function(e) {
+		$('splash-start').addClass('hide');
+		$('splash-log-in').removeClass('hide');
+		$('splash-sign-up').addClass('hide');
+		e.preventDefault();
+	});
+	
+	$('#nav-sign-up').click(function(e) {
+		$('splash-start').addClass('hide');
+		$('splash-log-in').addClass('hide');
+		$('splash-sign-up').removeClass('hide');
+		e.preventDefault();
+	});
+	
+	$('#nav-back').click(function(e) {
+		$('splash-log-in').addClass('hide');
+		$('splash-start').removeClass('hide');
+		$('splash-sign-up').addClass('hide');
+		e.preventDefault();
+	});
 });
 
 function appendError(element, message) {
