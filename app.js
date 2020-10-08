72cb98dd3f934953d0ba2521401523edfd1079c6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 21:34:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const create = React.createElement;
-const render = ReactDOM.render;
 
 function Welcome(props) {
-  return create("h1", null, "Hello, ", props.name);
+	return create("h1", null, "Hello, ", props.name);
 }
 
 function App() {
-  return create("div", null, /*#__PURE__*/React.createElement(Welcome, {
-    name: "Sara"
-  }), React.createElement(Welcome, {
-    name: "Cahal"
-  }), React.createElement(Welcome, {
-    name: "Edite"
-  }));
+	return create("div", null, /*#__PURE__*/React.createElement(Welcome, {
+		name: "Sara"
+	}), React.createElement(Welcome, {
+		name: "Cahal"
+	}), React.createElement(Welcome, {
+		name: "Edite"
+	}));
 }
 
-render( create(App, null), document.getElementById('root'));
+$(document).ready(function() {
+	ReactDOM.render( create(App, null), document.getElementById('root'));
+});
