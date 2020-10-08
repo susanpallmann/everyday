0a5404d921a902a800da50a8d48cca59db85e69a
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 21:59:04 -0400

    Update app-uncompiled.js

--- app-uncompiled.js
+++ app-uncompiled.js
@@ -2,6 +2,29 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
+// Creates Splash component
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+	}
+	
+	componentDidMount() {
+	}
+	
+	componentWillUnmount() {
+	}
+    
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
