f0eb9ad1582af36d6d9e0d84db6002ef2a95d119
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-07 22:48:17 -0400

    Update app-uncompiled.js

--- app-uncompiled.js
+++ app-uncompiled.js
@@ -22,10 +22,9 @@ class Splash extends React.Component {
 	}
     
 	render() {
-		let phase = this.state.phase;
 		return (
 			<div id="splash">
-				<h1>phase</h1>
+				<h1>{this.state.phase}</h1>
 			</div>
 		);
 	}
