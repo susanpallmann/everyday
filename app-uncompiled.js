c2cd4af3aa1cc79ed85fd0bea6ebef230a813f44
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-09 22:48:39 -0400

    Update app-uncompiled.js

--- app-uncompiled.js
+++ app-uncompiled.js
@@ -1,4 +1,6 @@
+// Verifying load order for testing (TODO: remove this)
 console.log("App file ready!");
+// Creates our bubble elements used in the day overview screen
 class Bubble extends React.Component {
 	render () {
 		return (
@@ -12,38 +14,35 @@ class Bubble extends React.Component {
 
 // Searches to see if an object in the array specified has the value provided set as the "key"
 function searchArray(word, array){
-	// For each item in the array
-	console.log(array);
-	console.log(word);
+	// For each item in the array (each category type)
 	for (var i=0; i < array.length; i++) {
-		console.log(array[i]);
-		// If the value under key "key" is equal to var keyword
+		// If the value under key "keyword" is equal to var word
 		if (array[i].keyword === word) {
 			// Returns the object
-			console.log(array[i]);
 			return array[i];
 		}
-// Else keeps iterating
 	}
 }
 
 // Function to render Day Overview section
+// React JS is giving me a warning here, but my code still works and I don't negotiate with terrorists. 
 function populateDayOverview(date, data) {
 	let dataElements = [];
+	// For each key/value passed in through parameter data (expected to be an object)
 	for (const [key, value] of Object.entries(data)) {
 		let keyword = key;
-		console.log(keyword);
 		let thisObject = searchArray(keyword, categories);
-		console.log(thisObject);
-		console.log(thisObject.title);
+		// Creates bubble from our class Bubble, sending in some object properties as props
 		let bubble = <Bubble 
 			propKey={keyword}
 			title={thisObject.title}
 			quant={thisObject.quant}
 			propValue = {value}
 		/>;
+	// Pushes created bubble elements into our array dataElements
 	dataElements.push(bubble);
 	}
+	// Renders the array of bubbles to the designated class
 	ReactDOM.render(
 		<div>{dataElements}</div>,
 		document.getElementById('day-info')
