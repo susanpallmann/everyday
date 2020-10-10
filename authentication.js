58f578903e1d4da03bd34dc5f749b2217a8cc9da
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-10 00:09:54 -0400

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -19,6 +19,7 @@ function checkLogInStatus() {
                 $('footer').fadeIn(200);
             }, 200);
             $('main').removeClass('hide');
+            initializeButtons();
         } else {
             // No user is signed in.
             if ($('main').hasClass('hide')) {
