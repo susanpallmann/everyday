e9aa3b20e21f7af1a060831b95eb04bc18c64aee
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-09 22:23:26 -0400

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,57 +1,55 @@
-$(document).ready(function() {
-	console.log("Categories file ready!");
-	var caffeine = {
-		keyword: "caffeine",
-		title: "Caffeine",
-		categIcon: "local_cafe",
-		icon: "local_cafe",
-		type: "number",
-		quant: "beverages"
-	};
-	var sleep = {
-		keyword: "sleep",
-		title: "Sleep",
-		categIcon: "bedtime",
-		icon: null,
-		type: "number",
-		quant: "hours"
-	};
+console.log("Categories file ready!");
+var caffeine = {
+	keyword: "caffeine",
+	title: "Caffeine",
+	categIcon: "local_cafe",
+	icon: "local_cafe",
+	type: "number",
+	quant: "beverages"
+};
+var sleep = {
+	keyword: "sleep",
+	title: "Sleep",
+	categIcon: "bedtime",
+	icon: null,
+	type: "number",
+	quant: "hours"
+};
 
-	var water = {
-		keyword: "water",
-		title: "Water",
-		categIcon: "local_drink",
-		icon: "local_drink",
-		type: "number",
-		quant: "cups"
-	};
+var water = {
+	keyword: "water",
+	title: "Water",
+	categIcon: "local_drink",
+	icon: "local_drink",
+	type: "number",
+	quant: "cups"
+};
 
-	var aaaMood = {
-		keyword: "aaaMood",
-		title: "Mood",
-		categIcon: "mood",
-		icon: "null",
-		type: "select",
-		options: [
-			"sentiment_very_dissatisfied",
-			"sentiment_dissatisfied",
-			"sentiment_satisfied",
-			"mood",
-			"sentiment_very_satisfied"
-		],
-		quant: [
-			"awful",
-			"poor",
-			"okay",
-			"good",
-			"great"
-		]
-	};
+var aaaMood = {
+	keyword: "aaaMood",
+	title: "Mood",
+	categIcon: "mood",
+	icon: "null",
+	type: "select",
+	options: [
+		"sentiment_very_dissatisfied",
+		"sentiment_dissatisfied",
+		"sentiment_satisfied",
+		"mood",
+		"sentiment_very_satisfied"
+	],
+	quant: [
+		"awful",
+		"poor",
+		"okay",
+		"good",
+		"great"
+	]
+};
 
-	var categories = [
-		caffeine,
-		sleep,
-		water,
-		aaaMood
-	];
-});
+var categories = [
+	caffeine,
+	sleep,
+	water,
+	aaaMood
+];
