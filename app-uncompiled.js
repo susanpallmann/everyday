35e3d066db1b7e1340dc7e4aa78236cd5fe6e6f6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-09 22:11:33 -0400

    Update app-uncompiled.js

--- app-uncompiled.js
+++ app-uncompiled.js
@@ -1,47 +1,52 @@
-// Empty
 $(document).ready(function() {
-  class Bubble extends React.Component {
-    render () {
-      <div class="bubble">
-        <h3 class={this.props.propKey + " bubble"}>{this.props.title}</h3>
-        <p>You logged {this.props.propValue} {this.props.quant} for {this.props.title.toLowerCase()}.</p>
-      </div>
-    }
-  }
+    class Bubble extends React.Component {
+		render () {
+			return (
+				<div className="bubble">
+					<h3 className={this.props.propKey}>{this.props.title}</h3>
+					<p>You logged {this.props.propValue} {this.props.quant} for {this.props.title.toLowerCase()}.</p>
+				</div>
+			);
+		}
+	}
 
-  // Searches to see if an object in the array specified has the value provided set as the "key"
-  function searchArray(keyword, array){
-    // For each item in the array
-    console.log(array);
-    for (var i=0; i < array.length; i++) {
-      // If the value under key "key" is equal to var keyword
-      if (array[i].key === keyword) {
-        // Returns the object
-        console.log(array[i]);
-        console.log(array[i].key);
-        return array[i];
-      }
+	// Searches to see if an object in the array specified has the value provided set as the "key"
+	function searchArray(word, array){
+		// For each item in the array
+		console.log(array);
+		console.log(word);
+		for (var i=0; i < array.length; i++) {
+			console.log(array[i]);
+			// If the value under key "key" is equal to var keyword
+			if (array[i].keyword === word) {
+				// Returns the object
+				console.log(array[i]);
+				return array[i];
+			}
       // Else keeps iterating
-    }
-  }
+		}
+	}
 
-  // Function to render Day Overview section
-  function populateDayOverview(date, data) {
-    let dataElements = [];
-    for (const [key, value] of Object.entries(data)) {
-      let keyword = key;
-      let thisObject = searchArray(keyword, categories);
-      let bubble = <Bubble 
-        propKey={keyword}
-        title={thisObject.title}
-        quant={thisObject.quant}
-        propValue = {value}
-      />;
-      dataElements.push(bubble);
-    }
-    ReactDOM.render(
-      <div>{dataElements}</div>,
-      document.getElementById('day-info')
-    );
-  }
+	// Function to render Day Overview section
+	function populateDayOverview(date, data) {
+		let dataElements = [];
+		for (const [key, value] of Object.entries(data)) {
+			let keyword = key;
+			console.log(keyword);
+			let thisObject = searchArray(keyword, categories);
+			console.log(thisObject);
+			console.log(thisObject.title);
+			let bubble = <Bubble 
+				propKey={keyword}
+				title={thisObject.title}
+				quant={thisObject.quant}
+				propValue = {value}
+			/>;
+		dataElements.push(bubble);
+		}
+		ReactDOM.render(
+			<div>{dataElements}</div>,
+			document.getElementById('day-info')
+		);
+	}
 });
