84a984c4a5d29f714295f247c42613d4e9918531
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-09 23:12:16 -0400

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -1,74 +1,5 @@
-/*function loadDay(value) {
-	console.log("clicked!");
-	let oldDate = $('#day-overview').find('h2').text();
-	let dateSeparated = oldDate.split("-");
-	console.log(dateSeparated);
-	let dateActual = new Date(parseInt(dateSeparated[0]), parseInt(dateSeparated[1]) - 1, parseInt(dateSeparated[2]));
-	let dateNext = dateActual.setDate(dateActual.getDate() + value );
-	let dateIntermediate = new Date(dateNext);
-	console.log(dateIntermediate);
-	let year = dateIntermediate.getFullYear();
-	let month = dateIntermediate.getMonth() + 1;
-	let day = dateIntermediate.getDate();
-	let dateFormatted = year + '-' + month + '-' + day;
-	readDay(dateFormatted, populateDataOverview);
-}*/
-
-function loadNextDay() {
-	console.log("clicked!");
-	let oldDate = $('#day-overview').find('h2').text();
-	let dateSeparated = oldDate.split("-");
-	console.log(dateSeparated);
-	let dateActual = new Date(parseInt(dateSeparated[0]), parseInt(dateSeparated[1]) - 1, parseInt(dateSeparated[2]));
-	let dateNext = dateActual.setDate(dateActual.getDate() + 1 );
-	let dateIntermediate = new Date(dateNext);
-	console.log(dateIntermediate);
-	let year = dateIntermediate.getFullYear();
-	let month = dateIntermediate.getMonth() + 1;
-	let day = dateIntermediate.getDate();
-	let dateFormatted = year + '-' + month + '-' + day;
-	readDay(dateFormatted, populateDataOverview);
-}
-/*function initializeDayButtons() {
-	console.log("initialized");
-	$('.next-day').click(function(e) {
-		let date = $('#day-overview').attr('day');
-		if (date !== "default") {
-			let dateSeparated = date.split("-");
-			let dateActual = new Date(dateSeparated[0], dateSeparated[1] - 1, dateSeparated[2]);
-			console.log(dateActual);
-			let dateNext = dateActual.setDate(dateActual.getDate() + 1 );
-			let year = dateNext.getFullYear();
-			let month = dateNext.getMonth() + 1;
-    			let day = dateNext.getDate();
-			let dateFormatted = year + '-' + month + '-' + day;
-			$('#day-overview').attr('day', dateFormatted);
-		} else {
-			console.log("else");
-		}
-		e.preventDefault();
-	});
-	
-	$('.previous-day').click(function(e) {
-		let date = $('#day-overview').attr('day');
-		if (date !== "default") {
-			let dateSeparated = date.split("-");
-			let dateActual = new Date(dateSeparated[0], dateSeparated[1] - 1, dateSeparated[2]);
-			console.log(dateActual);
-			let dateNext = dateActual.setDate(dateActual.getDate() - 1 );
-			let year = dateNext.getFullYear();
-			let month = dateNext.getMonth() + 1;
-    			let day = dateNext.getDate();
-			let dateFormatted = year + '-' + month + '-' + day;
-			$('#day-overview').attr('day', dateFormatted);
-		} else {
-			console.log("else");
-		}
-		e.preventDefault();
-	});
-}*/
-
 $(document).ready(function() {
+	// Changes splash view mode to display log in screen
 	$('.nav-log-in').click(function(e) {
 		$('#splash-start').addClass('hide');
 		$('#splash-log-in').removeClass('hide');
@@ -76,6 +7,7 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 	
+	// Changes splash view mode to display sign up screen
 	$('.nav-sign-up').click(function(e) {
 		$('#splash-start').addClass('hide');
 		$('#splash-log-in').addClass('hide');
@@ -83,6 +15,7 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 	
+	// Changes splash view mode to display main splash screen
 	$('.nav-back').click(function(e) {
 		$('#splash-log-in').addClass('hide');
 		$('#splash-start').removeClass('hide');
@@ -91,10 +24,12 @@ $(document).ready(function() {
 	});
 });
 
+// Appends error message (parameter message) to a specific element (parameter element)
 function appendError(element, message) {
 	element.append('<p class="error-message">' + message + '</p>');
 }
 
+// Removes error message element from specific element (parameter element)
 function clearError(element) {
 	element.find('.error-message').remove();
 }
