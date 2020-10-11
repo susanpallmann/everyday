43e7b305d834c00eb12a68dbd9068f0277c120a8
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-10 21:35:22 -0400

    Update firebase-scripts.js

--- firebase-scripts.js
+++ firebase-scripts.js
@@ -22,6 +22,26 @@ function updateDay(date, data, callback = nocallback) {
     });
 }
 
+// Sends information (parameter data, expected to be an object) about a specific date (parameter date) to the database
+function updateCategory(data, callback = nocallback) {
+
+    // First checks authentication to prevent the user from writing to any directory other than
+    // the one associated with their user ID.
+    var user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            let uid = user.uid;
+            // Update info at specific path
+            var path = firebase.database().ref('users/' + uid + '/settings/tracking');
+            path.update(data, function () {callback(true)});
+        } else {
+            // No user is signed in.
+            callback(false);
+        }
+    });
+}
+
 // Retrieves information from database about a specific date (parameter date), calls React script to load page (app.js)
 function readDay(date, callback) {
     $('#written-date').text(date);
@@ -82,8 +102,16 @@ $(document).ready(function() {
         "water": 4,
         "sleep": 8
     };
+    
+    
+    let data4 = {
+        "aaaMood": true,
+        "caffeine": false,
+        "water": true
+    };
     updateDay(date, data);
     updateDay("2020-10-11", data2);
     updateDay("2020-10-9", data3);
     readDay(date, populateDayOverview);
+    updateCategory(data4);
 });
