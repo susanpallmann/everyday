87846eaa35fda8e73d9f164b88bbbe0e7d33dd50
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2020-10-21 12:57:05 -0400

    Update app-uncompiled.js

--- app-uncompiled.js
+++ app-uncompiled.js
@@ -1,4 +1,85 @@
-/* Firebase-related functions we'll need later */
+/* ----------- OLD CODE (NEEDS REFACTORED FOR NEW BUILD TODO) ----------- */
+
+/* Firebase authentication functions we'll need later */
+/*
+function logOutUser() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
+
+// Observes to determine if user is logged in or not.
+function checkLogInStatus() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            $('#splash').fadeOut(200);
+            setTimeout(function(){
+                $('main').fadeIn(200);
+                $('header').fadeIn(200);
+                $('footer').fadeIn(200);
+            }, 200);
+            $('main').removeClass('hide');
+        } else {
+            // No user is signed in.
+            if ($('main').hasClass('hide')) {
+            } else {
+                $('main').fadeOut(200);
+                $('header').fadeOut(200);
+                $('footer').fadeOut(200);
+                setTimeout(function(){
+                    $('#splash').fadeIn(200);
+                }, 200);
+            }
+            $('#splash').fadeIn(200);
+        }
+    });
+}
+
+$(document).ready(function() {
+    $("#log-in").submit(function(e){
+        clearError($('#log-in'));
+        firebase.auth().signInWithEmailAndPassword($('#log-in-email').val(), $('#log-in-password').val()).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // Adds error message to page on log in/sign up screens
+            appendError($('#log-in').find('.error-container'), errorMessage);
+        });
+        e.preventDefault();
+    });
+  
+    $("#sign-up").submit(function(e){
+        clearError($('#sign-up'));
+        firebase.auth().createUserWithEmailAndPassword($('#sign-up-email').val(), $('#sign-up-password').val()).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // Adds error message to page on log in/sign up screens
+            appendError($('#sign-up').find('.error-container'), errorMessage);
+        });
+        e.preventDefault();
+    });
+  
+    $("#log-out").submit(function(e){
+        firebase.auth().signOut().then(function() {
+            $('#splash-log-in').addClass('hide');
+            $('#splash-start').removeClass('hide');
+            $('#splash-sign-up').addClass('hide');
+            // Sign-out successful.
+        }).catch(function(error) {
+            // An error happened.
+        });
+        e.preventDefault();
+    });
+    
+    checkLogInStatus();
+});
+*/
+
+/* Firebase realtime database-related functions we'll need later */
 /*function nocallback() {
 }
 
@@ -101,6 +182,8 @@ function readCategory(date, callback) {
     });
 }
 */
+
+/* ----------- BEGIN RECENT (GOOD) CODE ----------- */
 
 /* Some common elements we'll be reusing throughout the app */
 
